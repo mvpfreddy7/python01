077ee9d48e82c854a2bcf8a01e5e792edfc774ef
mvpfreddy7 <fsanchez9361@bths.edu>
2016-04-12 20:31:42 -0400

    revisions 3.3

--- Assignments/assignment_3.3_Freddy_Sanchez.py
+++ Assignments/assignment_3.3_Freddy_Sanchez.py
@@ -2,9 +2,9 @@
 gpa = raw_input("enter your GPA:")
 gpa = float(gpa)
 
-gpa > 0.0
-gpa < 1.0
-if gpa >= 0.9:
+if gpa < 0.0 or gpa > 1.0:
+	print "value out of range"
+elif gpa >= 0.9:
 	print "A"
 elif gpa >= 0.8:
 	print "B"
@@ -14,6 +14,6 @@ elif gpa >= 0.6:
 	print "D"
 elif gpa < 0.6:
 	print "F"
-else:
-	print "value out of range"
+
+	
 
