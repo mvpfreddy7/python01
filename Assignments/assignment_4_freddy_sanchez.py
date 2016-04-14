58eef07f57ce5ebff85ee6d4baff6566c0958c83
mvpfreddy7 <fsanchez9361@bths.edu>
2016-04-14 18:06:16 -0400

    revised assignment 4

--- Assignments/assignment_4_freddy_sanchez.py
+++ Assignments/assignment_4_freddy_sanchez.py
@@ -1,15 +1,16 @@
 #assignment_4.6
 def computepay (h,r):
-	return h*r
-
-h = raw_input("enter hours:")
-r = raw_input ("enter pay rate:")
-h = float(h)
-r = float(r)
-
-if h > 40:
-	def computepay(h,r):
+	try:
+		h = float(h)
+		r = float(r)
+	except:
+		return "input a number"
+	if h > 40:
 		return h*r + (h-40)*(r*1.5)-(h-40)*r
+	else:
+		return h*r
 
+ho = raw_input("enter hours:")
+ra = raw_input ("enter pay rate:")
 
-print "pay:", computepay (h,r)
+print "pay:", computepay (ho,ra)
